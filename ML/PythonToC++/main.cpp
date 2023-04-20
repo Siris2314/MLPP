bb088aa4d4c0321017ebccf9f5ab73dbdde8fe93
Siris2314 <tarihant2001@gmail.com>
2023-04-20 00:14:25 -0400

    Python to C++ Models

---
+++ ML/PythonToC++/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <torch/script.h>
+
+int main(){
+    torch::jit::script::Module net = torch::jit::load("../models/net.pt");
+    torch:: Tensor x = torch::randn({1,100});
+    std::vector<torch::jit::IValue> input;
+    input.push_back(x);
+    for(int i = 0; i<10; i++){
+        auto out = net.forward(input);
+        std::cout <<out <<std::endl;
+    }
+
+}
